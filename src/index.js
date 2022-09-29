2c191fd83bddebcab6a0d3b306840223bfd4e4f1
Dileep Meghwal <58825727+Dileepmeghwal@users.noreply.github.com>
2022-09-29 18:21:23 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,48 @@
+// destrucuture
+
+// const person = {
+//   id: 1,
+//   name: "Dileep",
+//   age: 23
+// }
+
+// console.log(person.id)
+// console.log(person.name)
+// console.log(person.age)
+
+// const person = {
+//   id: 1,
+//   name: "Dileep",
+//   age: 23
+// };
+
+// let { id, name, age } = person;
+
+// console.log(id, name, age);
+
+// const fruit = {
+//   fname: "Apple",
+//   price: 100,
+//   color: "Green"
+// };
+// let { fname, price, color } = fruit;
+// document.getElementById("root").innerHTML = `${fname} ${price} ${color}`;
+// console.log(fname,price,color)
+
+// rest Parameter
+
+// function myfun(a, b,...args){
+//   console.log(a);
+//   console.log(b);
+//   console.log(args)
+// }
+// myfun("Dileep","Meghwal","Engineer","Udaipur")
+
+// Spread Operator
+
+// let arr1 = ["one", "two"];
+// let arr2 = [...arr1,"three", "four", "five"];
+// console.log(arr2);
+
+const arrValue = ["My", "name", "is", "Jack"];
+console.log(arrValue);
